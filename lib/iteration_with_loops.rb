02fe0490b0382914a9c77fe28f2943e09790ece2
Israel Canessa <israellcanessa@gmail.com>
2020-07-06 20:11:12 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -14,8 +14,12 @@ end
 def find_min_in_nested_arrays(src)
   new_src = []
   row_index = 0
-  min_value = 100
-  while
+  while row_index < src.count do
+    element_index = 0 
+    min_value = 100
+    while element_index < src
+    end
+  end
 
 
   end
